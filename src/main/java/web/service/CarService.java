e0e8482eb25f550be622f32e64d65d9bc1aabfa8
AndreyVoynich <105066032+AndreyVoynich@users.noreply.github.com>
2022-05-27 00:52:15 +0300

    Update CarService.java

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,11 +1,8 @@
 package web.service;
 
 import web.model.Car;
-
 import java.util.List;
 
-public class CarService {
-    public int countCar(List<Car> list) {
-        return list.size();
-    }
+public interface CarService {
+    List<Car> getCars(int countCars);
 }
